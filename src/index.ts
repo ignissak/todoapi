2d16d1e80a92051f1fd7de130bd1616dc3ae6250
Jakub Bordáš <methalicity@gmail.com>
2021-11-28 14:56:18 +0100

    Production configuration changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { createConnection } from "typeorm";
 import { App } from "./app";
+import * as _ from '../config/default.json';
+import * as _1 from '../ormconfig.json';
 
 createConnection().then(connection => {
     console.log("Connected to database, starting server...");
